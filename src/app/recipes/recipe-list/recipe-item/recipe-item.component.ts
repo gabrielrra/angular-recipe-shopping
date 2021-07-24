f8bca59c38af7c22c9f000a5e238c870b11b50c4
Gabriel Amorim <gabrielrra.dev@gmail.com>
2021-07-24 14:52:58 -0300

    Recipe details on click added

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
+  @Output() onRecipeClicked = new EventEmitter<Recipe>();
+  @Input() recipeItem;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  showRecipeDetails(recipe: Recipe) {
+    this.onRecipeClicked.emit(recipe);
   }
-
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,11 +7,28 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() onRecipeItemSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('Chicken Parmigiana', 'Delicious fried chicken with tomato sauce and cheese', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-202102-airfryerchickenparm-180-ls-1612561654.jpg?crop=1.00xw:1.00xh;0,0&resize=980:*')
+    new Recipe(
+      'Chicken Parmigiana',
+      'Delicious fried chicken with tomato sauce and cheese',
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-202102-airfryerchickenparm-180-ls-1612561654.jpg?crop=1.00xw:1.00xh;0,0&resize=980:*'
+    ),
+    new Recipe(
+      'Cacio ao Pepe',
+      'Simple yet complex pasta',
+      'https://img.itdg.com.br/tdg/images/blog/uploads/2018/09/espaguete-cacio-e-pepe.jpg?w=1200'
+    ),
   ];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.onRecipeItemSelected.emit(this.recipes[0]);
+  }
+
+  showRecipeDetails(recipe: Recipe) {
+    this.onRecipeItemSelected.emit(recipe);
+  }
 }
